62559d09d6d8e226863578d7384a7743059515ee
Ed Scheinerman <ers@jhu.edu>
2024-01-10 12:21:34 -0500

    Results

--- test/speed_test_1.jl
+++ test/speed_test_1.jl
@@ -22,7 +22,7 @@ B = MiniMod.(A);
 
 nothing
 
-## RESULTS with widen()
+## RESULTS with widen()  &   UInt8
 # [ Info: Determinant tests
 #   1.105 s (4 allocations: 1.91 MiB)
 #   99.186 ms (128636 allocations: 89.81 MiB)
@@ -31,5 +31,30 @@ nothing
 # [ Info: Inverse tests
 #   5.729 s (8 allocations: 5.73 MiB)
 #   523.424 ms (254549 allocations: 262.71 MiB)
+# inv(A) == inv(B) = true
+# inv(B) == invx(B) = true
+
+
+## RESULTS with no widen() and UInt8
+# [ Info: Determinant tests
+#   1.196 s (4 allocations: 1.91 MiB)
+#   113.337 ms (128647 allocations: 89.81 MiB)
+# detx(A) == det(A) = true
+# det(A) == det(B) = true
+# [ Info: Inverse tests
+#   6.277 s (8 allocations: 5.73 MiB)
+#   635.274 ms (254560 allocations: 262.72 MiB)
+# inv(A) == inv(B) = true
+# inv(B) == invx(B) = true
+
+## RESULTS with no widen() and UInt16
+# [ Info: Determinant tests
+#   1.165 s (4 allocations: 1.91 MiB)
+#   131.794 ms (128679 allocations: 171.47 MiB)
+# detx(A) == det(A) = true
+# det(A) == det(B) = true
+# [ Info: Inverse tests
+#   6.055 s (8 allocations: 5.73 MiB)
+#   824.339 ms (254555 allocations: 507.96 MiB)
 # inv(A) == inv(B) = true
 # inv(B) == invx(B) = true
--- test/speed_test_2.jl
+++ test/speed_test_2.jl
@@ -17,10 +17,26 @@ C = value.(A);
 
 nothing
 
-## RESULTS with widen()
+## RESULTS with widen() & UInt8
 # [ Info: Testing matrix multiplication (Mod, MiniMod, Integer)
 #   21.916 s (6 allocations: 7.66 MiB)
 #   5.451 s (6 allocations: 1008.19 KiB)
 #   336.184 ms (9 allocations: 15.29 MiB)
+# A * A == B * B = true
+# A * A == (C * C) .% m = true
+
+## RESULTS without widen() and UInt8
+# [ Info: Testing matrix multiplication (Mod, MiniMod, Integer)
+#   21.850 s (6 allocations: 7.66 MiB)
+#   5.194 s (6 allocations: 1008.19 KiB)
+#   336.209 ms (9 allocations: 15.29 MiB)
+# A * A == B * B = true
+# A * A == (C * C) .% m = true
+
+## RESULTS with widen() and UInt16
+# [ Info: Testing matrix multiplication (Mod, MiniMod, Integer)
+#   23.041 s (6 allocations: 7.66 MiB)
+#   5.977 s (6 allocations: 1.94 MiB)
+#   350.945 ms (9 allocations: 15.29 MiB)
 # A * A == B * B = true
 # A * A == (C * C) .% m = true
